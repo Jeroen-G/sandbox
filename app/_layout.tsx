696ecdf12a06cf6c045a5a17edbe758cd283e53a
Jeroen <jgroenendijk@enrise.com>
2025-02-21 15:44:42 +0100

    fading splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from 'expo-router';
+import { setOptions as setSplashScreenOptions } from 'expo-splash-screen';
 import React from 'react';
+
+// SplashScreen.preventAutoHideAsync();
+setSplashScreenOptions({
+    duration: 1000,
+    fade: true,
+});
 
 export default function RootLayout() {
     return <Stack />;
