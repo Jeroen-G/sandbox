a8db2eb75366f19bfd0b31270b4d96f5b23f5b85
Jeroen <jgroenendijk@enrise.com>
2025-03-05 21:32:35 +0100

    morphing button

---
+++ app/buttons/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import { Morphing } from '@/components/Buttons/Morphing';
+
+export default function ButtonsScreen() {
+    return (
+        <SafeAreaView>
+            <Morphing />
+        </SafeAreaView>
+    );
+}
